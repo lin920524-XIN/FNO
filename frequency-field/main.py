4b8c2684146878853a54fcd9d7e024b1f4d714cd
lin920524-XIN <lin92052460@gmail.com>
2026-06-23 17:29:26 +0800

    Update main.py

--- frequency-field/main.py
+++ frequency-field/main.py
@@ -11,47 +11,64 @@ import FNO
 
 
 # 設定頻率數量與 frame 的切分比例
-NUM_SAMPLES = 200
+NUM_SAMPLES = 100
 RATIO = 0.4
 
 
 # 多個頻率的模擬
 X_all, Y_all, freq_list = generate_dataset(
-    n_samples=NUM_SAMPLES,
-    freq_min=1.0,
-    freq_max=5.0,
-    total_meep_time=100,
-    num_frames=100,
-    resolution=333,
-    seed=42,
-    ratio=RATIO,
-    save_path_X="dataset_X.npy",
-    save_path_Y="dataset_Y.npy",
-    save_path_freq="dataset_freq.npy",
-    verbose=True
+    n_samples        = NUM_SAMPLES,          
+    
+    freq_min         = 1.0,
+    freq_max         = 5.0,
+    
+    total_meep_time  = 100,
+    num_frames       = 100,         
+    
+    resolution       = 333,
+    
+    seed             = 42,
+    
+    ratio            = RATIO,
+    
+    save_path_X      = f"dataset_X_{NUM_SAMPLES}.npy",
+    save_path_Y      = f"dataset_Y_{NUM_SAMPLES}.npy",
+    save_path_freq   = f"dataset_freq_{NUM_SAMPLES}.npy",
+    
+    verbose          = True,
 )
 
 
 # 設定 FNO 模型參數
-CFG = {
-    "path_X": "dataset_X.npy",
-    "path_Y": "dataset_Y.npy",
-    "val_ratio": 0.15,
-    "n_modes": (12, 12),
-    "n_layers": 4,
-    "hidden_channels": 24,
-    "in_channels": None,
-    "out_channels": None,
-    "batch_size": 4,
-    "lr": 1e-3,
-    "weight_decay": 1e-4,
-    "scheduler_step": 60,
-    "scheduler_gamma": 0.5,
-    "epochs": 100,
-    "seed": 42,
-    "device": "cpu",
-    "save_dir": "checkpoints"
-}
+CFG = dict( path_X          = f"dataset_X_{NUM_SAMPLES}.npy",
+            path_Y          = f"dataset_Y_{NUM_SAMPLES}.npy",
+            
+            train_ratio     = 0.7,
+            val_ratio       = 0.15,
+            
+            n_modes         = (12, 12),
+            n_layers        = 4,
+            
+            hidden_channels = 24,
+            in_channels     = None,
+            out_channels    = None,
+            
+            batch_size      = 4,
+            
+            lr              = 1e-3,
+            
+            weight_decay    = 1e-4,
+            scheduler_step  = 60,
+            scheduler_gamma = 0.5,
+            
+            epochs          = 150,
+            
+            seed            = 42,
+            
+            device          = "cpu",
+            
+            save_dir        = f"checkpoints_{NUM_SAMPLES}",
+          )
 
 # FNO 模型的訓練
 model, history, test_loader = FNO.train(CFG)
